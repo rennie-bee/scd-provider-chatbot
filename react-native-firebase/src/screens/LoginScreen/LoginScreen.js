cdb6a7bf2ffee47bdb63c0ad568d2cf3822b4db0
ahcjla <aureliaxxaudrey@gmail.com>
2024-02-24 16:36:13 -0800

    Updated react native firebase

--- react-native-firebase/src/screens/LoginScreen/LoginScreen.js
+++ react-native-firebase/src/screens/LoginScreen/LoginScreen.js
@@ -12,6 +12,30 @@ export default function LoginScreen({navigation}) {
     }
 
     const onLoginPress = () => {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                const uid = response.user.uid
+                const usersRef = firebase.firestore().collection('users')
+                usersRef
+                    .doc(uid)
+                    .get()
+                    .then(firestoreDocument => {
+                        if (!firestoreDocument.exists) {
+                            alert("User does not exist anymore.")
+                            return;
+                        }
+                        const user = firestoreDocument.data()
+                        navigation.navigate('Home', {user})
+                    })
+                    .catch(error => {
+                        alert(error)
+                    });
+            })
+            .catch(error => {
+                alert(error)
+            })
     }
 
     return (
