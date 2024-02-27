eaa604580c757f609fd1af2ac06ba01b0ea80d5f
ahcjla <aureliaxxaudrey@gmail.com>
2024-02-27 13:17:58 -0800

    trial 2 firebase

--- react-native-firebase/src/firebase/config.js
+++ react-native-firebase/src/firebase/config.js
@@ -1,19 +1,19 @@
-import * as firebase from 'firebase';
-import '@firebase/auth';
-import '@firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBUs7pA3UVTbBrwfWqnrUVNwSlYRZb2DPc',
-  authDomain: 'scd-chatbot.firebaseapp.com',
-  databaseURL: 'https://scd-chatbot-default-rtdb.firebaseio.com',
-  projectId: 'scd-chatbot',
-  storageBucket: 'scd-chatbot.appspot.com',
-  messagingSenderId: '963935715708',
-  appId: '1:963935715708:ios:835819d01bbc11b1a8a4f9',
+  apiKey: "AIzaSyDMdsOdPr2dzUT8-34g1xElSi2lrzOKLi0",
+  authDomain: "scd-chatbot.firebaseapp.com",
+  databaseURL: "https://scd-chatbot-default-rtdb.firebaseio.com",
+  projectId: "scd-chatbot",
+  storageBucket: "scd-chatbot.appspot.com",
+  messagingSenderId: "963935715708",
+  appId: "1:963935715708:ios:835819d01bbc11b1a8a4f9",
 };
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
-export { firebase };
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
