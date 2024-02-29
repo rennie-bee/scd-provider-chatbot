c6e34f14bfa8e0407ae3ab40970d64b71e890131
sqz0914 <sqz0914@gmail.com>
2024-02-28 18:14:48 -0800

    improve ui
    1. Set theme to cupcake
    2. Change drag & drop zone background color

--- admin-portal/app/upload/page.tsx
+++ admin-portal/app/upload/page.tsx
@@ -145,13 +145,13 @@ const UploadPage: NextPage = () => {
           {/* drag and drop zone */}
           <div onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave} className="flex items-center justify-center w-full pl-[25%] pr-[25%]">
             <label htmlFor="dropzone-file" className={`flex flex-col items-center justify-center w-full h-80 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer ${
-    isDragOver ? 'border-zinc-500' : 'border-gray-300'} bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600`}>
+    isDragOver ? 'border-zinc-500' : 'border-gray-300'} bg-neutral-50  hover:bg-neutral-100`}>
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                 </svg>
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">{'TXT, DOCX, PDF or XLSX (MAX 100MB)'}</p>
+                <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                <p className="text-xs text-gray-500">{'TXT, DOCX, PDF or XLSX (MAX 100MB)'}</p>
               </div>
             </label>
             <input ref={fileInputRef} id="dropzone-file" type="file" name="file" className="hidden" onChange={handleFileChange} />
--- admin-portal/tailwind.config.ts
+++ admin-portal/tailwind.config.ts
@@ -17,7 +17,8 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "dark", "cupcake", "fancy"],
+    darkTheme: "cupcake",
   },
 }
 export default config
