be0b295bcd2036ac0c3d76caac64819339084eae
sqz0914 <sqz0914@gmail.com>
2024-03-06 18:23:26 -0800

    Update app.py

--- flask-server/app.py
+++ flask-server/app.py
@@ -26,6 +26,10 @@ def simple_chatbot_logic(user_input):
         return "Hello! How can I help you today?"
     elif user_input.lower() == 'bye':
         return "Goodbye! Have a great day!"
+    elif user_input.lower() == 'thank you':
+        return "You're welcome"
+    elif user_input.lower() == 'what is scd?':
+        return "Sickle cell disease (SCD) and its variants are genetic disorders resulting from the presence of a mutated form of hemoglobin, hemoglobin S (HbS). The most common form of SCD found in North America is homozygous HbS disease (HbSS), an autosomal recessive disorder first described by Herrick in 1910. SCD causes significant morbidity and mortality, particularly in people of African and Mediterranean ancestry. Morbidity, frequency of crisis, degree of anemia, and the organ systems involved vary considerably from individual to individual."
     else:
         return "I'm not sure how to respond to that. Can you try asking something else?"
 
